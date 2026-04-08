7eb701e9d5101a84a578a931269635b044502e97
1DASTAM1 <21dastam12@gmail.com>
2026-04-08 16:24:02 +0300

    Update Circular_Queue2.cpp

--- Circular_Queue2.cpp
+++ Circular_Queue2.cpp
@@ -1,7 +1,11 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <queue>
-#include "Circular_Queue.h"
+#include <vector>
 
 using namespace std;
+
+namespace py = pybind11;
 
 struct CircularQueue {
     queue<int> Circular_Queue;
@@ -30,10 +34,8 @@ bool isFull(CircularQueue *cq) {
 void Enqueue(CircularQueue *cq, int element) {
     if(isFull(cq)) {
         cq->Circular_Queue.pop();
-        cq->Circular_Queue.push(element);
-    } else {
-        cq->Circular_Queue.push(element);
     }
+    cq->Circular_Queue.push(element);
 }
 
 int Dequeue(CircularQueue *cq, bool *error) {
@@ -47,7 +49,8 @@ int Dequeue(CircularQueue *cq, bool *error) {
     return element;
 }
 
-void check(CircularQueue *cq, bool *Empty, int *item) {
+vector<int> check(CircularQueue *cq, bool *Empty) {
+    vector<int> res;
     *Empty = false;
     if (isEmpty(cq)) {
         *Empty = true;
@@ -55,10 +58,11 @@ void check(CircularQueue *cq, bool *Empty, int *item) {
     else {
         queue<int> qqq = cq->Circular_Queue;
         for (int i = 0; i < cq->Circular_Queue.size(); i++) {
-            item[i] = qqq.front();
+            res.push_back(qqq.front());
             qqq.pop();
         }
     }
+    return res;
 }
 
 void DeleteCircularQueue(CircularQueue *cq) {
@@ -69,4 +73,34 @@ void DeleteCircularQueue(CircularQueue *cq) {
 
 int SizeCircularQueue(CircularQueue *cq) {
     return static_cast<int>(cq->Circular_Queue.size());
+}
+
+pair<CircularQueue*, bool> Qcreate(int size) {
+    bool error;
+    CircularQueue* cq = CreateCircularQueue(size, &error);
+    return {cq, error};
+}
+
+pair<int, bool> Qdequeue(CircularQueue* cq) {
+    bool error;
+    int val = Dequeue(cq, &error);
+    return {val, error};
+}
+
+pair<vector<int>, bool> Qcheck(CircularQueue* cq) {
+    bool empty;
+    vector<int> qqq = check(cq, &empty);
+    return {qqq, empty};
+}
+
+PYBIND11_MODULE(Circular_Queue2, m) {
+    py::class_<CircularQueue>(m, "CircularQueue")
+        .def("CreateCircularQueue", &Qcreate)
+        .def("isEmpty", &isEmpty)
+        .def("isFull", &isFull)
+        .def("Enqueue", &Enqueue)
+        .def("Dequeue", &Qdequeue)
+        .def("check", &Qcheck)
+        .def("SizeCircularQueue", &SizeCircularQueue)
+        .def("DeleteCircularQueue", &DeleteCircularQueue);
 }
